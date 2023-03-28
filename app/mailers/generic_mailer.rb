a89bb3f5b4102140062288eb126393019ec9d69b
vaibhavbajaj <vaibhavbajaj15@gmail.com>
2023-03-28 00:19:20 -0500

    corrected 'from' in email functionality

--- app/mailers/generic_mailer.rb
+++ app/mailers/generic_mailer.rb
@@ -26,10 +26,11 @@ class GenericMailer < ApplicationMailer
     opts[:attachments].each { |filename, file| attachments[filename] = file } unless opts[:attachments].nil?
 
     logger.debug 'HERE'
-    #logger.debug from
+    logger.debug from
     logger.debug email_address_with_name(recipient.email, recipient.full_name)
 
     mail(
+      from: from[0],
       to: email_address_with_name(recipient.email, recipient.full_name),
       cc: from,
       subject: subject
