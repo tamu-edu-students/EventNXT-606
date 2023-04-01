e6d0d1ec274c3b772466c3949e875ddef90e0059
Sushmita <36688322+spattanaik9@users.noreply.github.com>
2023-03-31 19:11:30 -0500

    steps added for add guest feature test

--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -1,24 +1,47 @@
 # * http://benmabey.com/2008/05/19/imperative-vs-declarative-scenarios-in-user-stories.html
 # * http://dannorth.net/2011/01/31/whose-domain-is-it-anyway/
 # * http://elabs.se/blog/15-you-re-cuking-it-wrong
-Given /^the following events exist$/ do |table|
-  require 'date'
-  table.hashes.each { |h|
-    h['datetime'] = Date.parse h['datetime']
-    h['last_modified'] = Date.parse h['last_modified']
-    create :event, h
-  }
+# Given /^the following events exist$/ do |table|
+#   require 'date'
+#   table.hashes.each { |h|
+#     h['datetime'] = Date.parse h['datetime']
+#     h['last_modified'] = Date.parse h['last_modified']
+#     create :event, h
+#   }
+# end
+
+Given(/^the following events exist$/) do |table|
+  table.hashes.each do |event|
+    # code to create event in the database
+    Event.create event
+  end
 end
 
-Given /^I access "(.+)"$/ do |url|
-  @url = url
+# Given /^I access "(.+)"$/ do |url|
+#   @url = url
+# end
+
+# When /^I post an image parameter$/ do
+#   event = build :event
+#   @response = patch @url, params: { image: event.image }
+# end
+
+# Then /^I should get a successful response$/ do
+#   expect(@response).to be_successful
+# end
+
+Given(/^I access "(.*?)"$/) do |url|
+  visit url
 end
 
-When /^I post an image parameter$/ do
-  event = build :event
-  @response = patch @url, params: { image: event.image }
+When(/^I fill field "(.*?)" with "(.*?)"$/) do |field, value|
+  fill_in field, with: value
 end
 
-Then /^I should get a successful response$/ do
-  expect(@response).to be_successful
+When(/^I click on "(.*?)"$/) do |button|
+  click_on button
+end
+
+Then(/^I should get a successful response$/) do
+  expect(page.status_code).to eq(200)
 end
