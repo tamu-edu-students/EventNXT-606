d439bed6ba54a97e4e4cc47a4125e4fce9c4f17c
vaibhavbajaj <vaibhavbajaj15@gmail.com>
2023-04-06 02:50:30 -0500

    postgres int type casting in referral table

--- db/migrate/20230406042221_change_counted_to_be_integer_in_guest_referrals.rb
+++ db/migrate/20230406042221_change_counted_to_be_integer_in_guest_referrals.rb
@@ -1,5 +1,5 @@
 class ChangeCountedToBeIntegerInGuestReferrals < ActiveRecord::Migration[7.0]
   def change
-      change_column :guest_referrals, :counted, :integer
+      change_column :guest_referrals, :counted, 'integer USING CAST(counted AS integer)'
   end
 end
