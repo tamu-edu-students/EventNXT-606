185b3c73c87ec9138696423e9c9aadf7d36e3929
vaibhavbajaj <vaibhavbajaj15@gmail.com>
2023-04-06 16:15:02 -0500

    add event id to guest refferl

--- db/migrate/20230406082808_remove_counted_from_guest_referrals.rb
+++
@@ -1,6 +0,0 @@
-class RemoveCountedFromGuestReferrals < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :guest_referrals, :counted
-    add_column :guest_referrals, :counted, :integer, default: 0, null: false
-  end
-end
