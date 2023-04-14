b89101e3cb2cc80c206d6115c4f4d698b92faa8b
Amin <37824371+Aminisazadeh@users.noreply.github.com>
2023-04-14 18:01:17 -0500

    Create create_event_steps.rb

---
+++ features/step_definitions/create_event_steps.rb
@@ -0,0 +1,37 @@
+Given("I am logged in as a registered user") do
+  visit root_path
+  fill_in "login-input-email", with: "isazadeh.biz@gmail.com"
+  fill_in "login-input-password", with: "12345"
+  click_button "Log in"
+end
+
+Given("I am on the event dashboard path") do
+  visit events_path
+end
+
+When("I click on the {string} button") do |button_text|
+  click_on button_text
+end
+
+Then("I should see a form with id {string} pop up") do |form_id|
+  expect(page).to have_css("##{form_id}")
+end
+
+When("I fill in the following details:") do |table|
+  details = table.rows_hash
+  fill_in "event_name", with: details["event_name"]
+  fill_in "event_Address", with: details["event_Address"]
+  fill_in "event_Description", with: details["event_Description"]
+  fill_in "event_datetime", with: details["event_datetime"]
+end
+
+When("I click on the {string} button") do |button_text|
+  click_on button_text
+end
+
+Then("I should see the new event added to the dashboard path") do
+  expect(page).to have_content("My Event")
+  expect(page).to have_content("123 Main St")
+  expect(page).to have_content("A fun event for all")
+  expect(page).to have_content("April 30, 2023 12:00 PM")
+end
